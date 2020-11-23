40ce3fbb3dac734adc4efaf310e29ad68710233b
xiexyong <xiexyong@users.noreply.github.com>
2020-11-23 09:25:20 +0700

    init project

---
+++ src/com/dung/userinterface/GameFrame.java
@@ -0,0 +1,31 @@
+package com.dung.userinterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+
+    public static final int s_width = 1000;
+    public static final int s_height = 600;
+    GamePanel gamePanel;
+
+    public GameFrame(){
+        Toolkit toolkit = this.getToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        this.setBounds((dimension.width-s_width)/2,(dimension.height-s_height)/2,s_width,s_height);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gamePanel = new GamePanel();
+        add(gamePanel);
+    }
+
+    public void startGame(){
+        gamePanel.startGame();
+    }
+
+    public static void main(String[] args) {
+        GameFrame gameFrame = new GameFrame();
+        gameFrame.setVisible(true);
+        gameFrame.startGame();
+    }
+}
---
+++ src/com/dung/userinterface/GamePanel.java
@@ -0,0 +1,43 @@
+package com.dung.userinterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    private Thread thread;
+
+    private boolean isRunning;
+    @Override
+    public void paint(Graphics g){
+        g.setColor(Color.red);
+        g.fillRect(0,0,GameFrame.s_width,GameFrame.s_height);
+
+    }
+    public void startGame(){
+        if(thread == null){
+            isRunning = true;
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+    @Override
+    public void run() {
+        long FPS = 120;
+        long period = 1000*1000000/FPS;
+        long beginTime;
+        long sleepTime;
+        beginTime = System.nanoTime();
+        int a = 1;
+        while(isRunning){
+//                System.out.println("a = "+(a++));
+//                long deltaTime = System.nanoTime() - beginTime;
+//                sleepTime = period - deltaTime;
+//            try {
+//                if (sleepTime>0) Thread.sleep(sleepTime/1000000);
+//                else Thread.sleep(sleepTime/2000000);
+//            } catch (InterruptedException e) { }
+            System.out.println("a = "+(a++));
+        }
+    }
+}
